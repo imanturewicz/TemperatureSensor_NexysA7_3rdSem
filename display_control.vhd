b88743bb173d1bd4fae22ddae03ee80225a117d2
imanturewicz <ignacy.manturewicz@gmail.com>
2025-01-08 19:51:08 +0100

    Adding some comments

--- display_control.vhd
+++ display_control.vhd
@@ -7,7 +7,7 @@ entity display_control is
         CLK_in: in std_logic;
         F_in: in std_logic;
         temperatura_in: in integer range 0 to 10000;
-        th_out: out integer range 0 to 9;
+        th_out: out integer range 0 to 9;        --digits to display (4th, 3rd, 2nd, 1st)
         rd_out: out integer range 0 to 9;
         nd_out: out integer range 0 to 9;
         st_out: out integer range 0 to 9
@@ -20,7 +20,7 @@ begin
     TempChange: process(Clk_in)
     begin
         if rising_edge(Clk_in) then
-            temperaturef <= temperatura_in * 9 / 5 + 3200;
+            temperaturef <= temperatura_in * 9 / 5 + 3200;        --changing the temperature to degrees of Fahrenhait multiplied by 100
         end if;
     end process TempChange;
 
@@ -28,7 +28,7 @@ begin
         variable temp1: integer;
         variable temp2: integer;
     begin
-        if rising_edge(CLK_in) then
+        if rising_edge(CLK_in) then        --parsing the tens, units, tenth and hundredth parts
             if F_in = '0' then
                 th_out <= temperatura_in / 1000;
                 temp1 := temperatura_in - (1000 * (temperatura_in/1000));
@@ -47,3 +47,6 @@ begin
         end if;
     end process Control;
 end disp_cntrl_arch;
+--The Display Controller entity is responsible for manipulating the results obtained from the temperature sensor.
+--It takes the temperature reading (in degrees of Celsius multiplied by one hundred) as an input.
+--It outputs four decimal digits to be shown on the display. The scale in which the result is passed depends on the switch.
